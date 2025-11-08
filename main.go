7a9e8368ea60cc4f5944e76af6fb79b5c2b27ab9
Loren Barcus <loren.barcus@gmail.com>
2025-11-08 08:10:39 -0600

    Add health endpoint. Refactor file serving code. Fix hard-coded path bug

--- main.go
+++ main.go
@@ -1,33 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-type MyRootHanlder struct{}
+type RootHanlder struct{}
 
 // Implements the http.Handler interface directly in the struct.
 // This method's signature exactly matches the interface requirement.
-func (h *MyRootHanlder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// write contents of index.html
-	// http.FileServer(http.Dir("."))
-	http.ServeFile(w, r, "index.html")
-	// fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+func (h *RootHanlder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// r.URL.Path now has "/app" stripped
+	filepath := r.URL.Path
+
+	// If path is empty or just "/", serve index.html
+	if filepath == "" || filepath == "/" {
+		filepath = "/index.html"
+	}
+
+	// Remove leading slash to make it relative to current directory
+	filepath = filepath[1:] // Remove the leading "/"
+
+	fmt.Printf("Serving file: %v\n", filepath)
+	http.ServeFile(w, r, filepath)
+}
+
+type HealthHanlder struct{}
+
+func (h *HealthHanlder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(`OK`))
 }
 
 func main() {
+	const port = "8080"
 	mux := http.NewServeMux()
-	server := http.Server{}
-	server.Addr = ":8080"
-	server.Handler = mux
 
-	// 1. Create instance of the struct.
-	// The variable 'handler' is now a value of type *MyRootHandler,
-	// and it statifies the http.Handler interface.
-	handler := &MyRootHanlder{}
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
 
-	mux.Handle("/", handler)
+	// fileServer := http.FileServer(http.Dir("."))
+	// mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.Handle("/app/", http.StripPrefix("/app", &RootHanlder{}))
+	mux.Handle("/healthz", &HealthHanlder{})
 	log.Println("Starting server on :8080")
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(srv.ListenAndServe())
 }
